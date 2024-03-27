9907363455013d23d36bfa6d18258a147f8845ae
Martin Andersson <martin.andersson111@gmail.com>
2024-03-27 08:37:11 +0000

    removed unused functions and shifted integration limits

--- src/SPDE.jl
+++ src/SPDE.jl
@@ -33,66 +33,6 @@ function L_op(u,dt,dx)
     return Lu
 end
 
-
-function alternate_method(u,dt,dx)
-    Lu = L_op(u,dt,dx)
-end
-
-function integrate(Lu,dx,dt,ϵ)
-    nx,nt = size(Lu)
-    x_dims,t_dim = size(Lu)
-    x_eps = (ϵ ÷ dx) |> Int 
-    t_eps = (ϵ ÷ dt) |> Int
-    #size(Lu)
-    x0 = nx ÷ 2 |> Int
-    #@show x0 + x_eps
-    t0 = 1
-    integration_matrix = Lu[x0-x_eps:x0+x_eps,t0:t0+t_eps]
-    integrated = (1/ϵ*dt*dx*sum(integration_matrix[2:end-1,1:end-1]))^2
-    return integrated
-end
-
-function one_loop(u,dt,dx,ϵ)
-    nx,nt = size(u)
-    x_eps = (ϵ ÷ dx) |> Int 
-    t_eps = (ϵ ÷ dt) |> Int
-    t0=1
-    x0 = nx ÷ 2 |> Int
-    u = u[x0-x_eps-1:x0+x_eps+1,t0:t0+t_eps+1]
-    u0 = u[x_eps,t0]
-    Lu = L_op(u,dt,dx)
-    sample = integrate(Lu,dx,dt,ϵ)
-    return u0,sample
-end
-
-function whole_thing(ensemble,dt,dx,eps,ϵ)
-    t_length= ensemble[1].t |> length
-    data = []
-    @show x_eps = (eps ÷ dx) |> Int 
-    @show t_eps = (eps ÷ dt) |> Int
-    @show x_ϵ = (ϵ ÷ dx) |> Int 
-    @show t_ϵ = (ϵ ÷ dt) |> Int
-    num_x_samples = 20
-    total_samples = min(50000,t_length)
-    factor = t_length*num_x_samples/total_samples |> x-> ceil(Int,x)
-    for sol in ensemble
-        Lu = L_op(sol,dt,dx)
-        x_length = size(Lu)[1]
-        x_mid = x_length ÷ 2 |> Int
-        for t in 1:factor:t_length-t_eps
-                for x0 in 1:num_x_samples #x_eps+3:10:x_length-x_eps-3
-                    x0 = rand(10:x_length-10)
-                    #sol_copy = @views sol[:,t:t+t_eps+1]
-                    #Lu_copy = @views Lu[:,t:t+t_eps+1]
-                    Lu_copy = Lu[x0:x0+x_eps-1,t:t+t_eps-1] #Lu[x0-x_eps:x0+x_eps,t:t+t_eps]
-                    u = sol[x0,t]
-                    integrated = (1/eps*dt*dx*sum(Lu_copy))^2
-                    push!(data,(u,integrated))
-                end
-        end
-    end
-    return data
-end
 function downsample_matrix(matrix, fx, fy)
     # Downsample a matrix by averaging over fx by fy blocks
     Nx, Ny = size(matrix)
@@ -180,7 +120,7 @@ function partial_integration(solution,dt,dx,L,tmax,x_quot,t_quot,eps)
         for i in 1:num_x_samples
             #x = rand(x_eps+1:x_len-x_eps-1)
             x=rand_x[i]
-            integrated_view = view(Lu, x:x+x_eps+1, t:t+t_eps+1) # right now not shifted to -1 to compensate
+            integrated_view = view(Lu, x:x+x_eps, t:t+t_eps) # right now not shifted to -1 to compensate
             l1,l2 = size(integrated_view)
             integrated=trapz((1:l1,1:l2),integrated_view)^2
            #integrated = sum(integrated_view)^2
@@ -197,84 +137,6 @@ function partial_integration(solution,dt,dx,L,tmax,x_quot,t_quot,eps)
     df = DataFrame(:x=>first.(collected_results),:y=>last.(collected_results))
     #zipped=zip(new_sol[buffer:end-buffer,ks:end],Lu[buffer:end-buffer,ks:end]) |> collect |> x->reshape(x,:)
     return df
-end
-
-
-
-
-function experiment_2(solution,dt,dx,L,tmax,x_quot,t_quot,eps,sigma)
-	df = partial_integration(solution,dt,dx,L,tmax,x_quot,t_quot,eps)
-	@show x_size = size(df,1)
-	if x_size < 50
-		return nothing
-	end
-    df_train,df_test = partition(df,0.8,rng=123)
-    σ = sigma[1]
-    sigma_name = sigma[2]
-	new_dx = x_quot*dx
-	new_dt = t_quot*dt
-	#df=DataFrame(Dict(:x=>first.(data),:y=>last.(data)))
-	x_data = select(df_train,1)
-	y_data = df_train.y #.* nx/t_quot
-	#@show size(df)
-	max_nbors = min(300,length(df.x))
-    knn=KNN()
-    r=range(knn,:K,lower=20,upper=max_nbors)
-    knn_tuned = TunedModel(model=knn,resampling=CV(nfolds=3),
-    tuning=Grid(resolution=20), range=r,measure=l2)
-	#gridsearch=GridSearchCV(KNeighborsRegressor(),Dict(:n_neighbors=>20:20:max_nbors));
-	#ScikitLearn.fit!(gridsearch,x_data[:,:],y_data);
-    mach = machine(knn_tuned,x_data,y_data)
-    fit!(mach)
-	if size(x_data,1) < 15000
-		x = x_data
-	else
-        samples_idxs = sample(1:size(df_train, 1), 10000, replace=false)
-		x=x_data[samples_idxs,:] 
-	end
-    x=select(df_test,1)
-	#p1=scatter(df.x,df.y,alpha=0.1,label=L"(L^hu)^2")
-	#plot!(x,x->σ(x)^2,label=L"\sigma^2",linewidth=3.,title="estimates")
-	#plot!(x,predict(gridsearch,x[:,:]),label="K-nearest neighbor",linewidth=3.)
-	#lims!(0,3)
-	#p2=scatter(σ.(x).^2 ./ predict(gridsearch,x[:,:]),label="quotient")
-	#scatter!(df.x,df.y,alpha=0.1)
-
-	l1_loss = abs.(σ.(x.x).^2 .- predict(mach,x)) |> mean
-	l1_std = abs.(σ.(x.x).^2 .- predict(mach,x)) |> std
-	l2_loss = abs.(σ.(x.x).^2 .- predict(mach,x)).^2 |> mean
-	l2_std = abs.(σ.(x.x).^2 .- predict(mach,x)).^2 |> std
-	quotient = (σ.(x.x).^2 ./ predict(mach,x)) |> x->filter(!isnan,x) |> mean
-	return (1,(eps,new_dx,new_dt,l1_loss,l1_std,l2_loss,l2_std,quotient,x_size,sigma_name))
-end
-
-function experiment_3(u_begin,nx,samples,dt,dx,L,tmax,algo,t_idxs,sigmas)
-	df_loss = DataFrame([:eps=>Float64[],:new_dx=>Float64[],:new_dt=>Float64[],:l1_loss=>Float64[],:l1_std=>Float64[],:l2_loss=>Float64[],:l2_std=>Float64[],:quotient=>Float64[],:x_size=>Float64[],:sigma=>String[]])
-	drift,diff = get_drift_diffusion()
-    jac_prot = Tridiagonal([1.0 for i in 1:nx-1],[-2.0 for i in 1:nx],[1.0 for i in 1:nx-1]) |> sparse
-	SDE_sparse = SDEFunction(drift,diff;jac_prototype=jac_prot)
-    t0=tmax;
-    ϵ = 32dx
-    time_range = ((0.0,t0+ϵ+dt))
-	#σ = sigma
-	#p=(dx,σ)
-	#E=EnsembleProblem(SDEProblem(SDE_sparse,u_begin,time_range,p))
-	for i in 1:samples, sigma in sigmas
-		#E=E_sigma[1]
-		#sigma = E_sigma[2]
-		σ = sigma[1]
-		sigma_name = sigma[2]
-		p=(dx,σ)
-		E=EnsembleProblem(SDEProblem(SDE_sparse,u_begin,time_range,p))
-		solution=solve(E,dtmax=dt,trajectories=1,saveat=t_idxs,progress=true,algo,maxiters=1e7)[1]
-        println(solution.t[end])
-		for j in 0:4#0:4
-			res=[experiment_2(solution,dt,dx,L,tmax,2^i,2^i*3,(2^(i+j))*dx,sigma) for i in 7:-1:0] #7:-1:0
-			filter!(!isnothing,res)
-			push!(df_loss,last.(res[:])...)
-		end
-	end
-	return df_loss
 end
 
 function monte_carlo_error(mach,truth,domain,N,loss)
@@ -412,29 +274,3 @@ function train_tuned(df)
 end
 
 end
-#=
-function whole_thing_lamperti(sol,dt,dx,eps,ϵ)
-    x_length,t_length = size(sol)
-    data = []
-    @show x_eps = (eps ÷ dx) |> Int 
-    @show t_eps = (eps ÷ dt) |> Int
-    @show x_ϵ = (ϵ ÷ dx) |> Int 
-    @show t_ϵ = (ϵ ÷ dt) |> Int
-    #sol = ψ_inv.(Array(sol))
-    Lu = L_op(sol,dt,dx)
-    x_length = size(Lu)[1]
-    @show x0 = x_length ÷ 2 |> Int
-    for t in 100:1000:t_length-t_eps-1
-            for x0 in [x0]#x_eps+3:1:x_length-x_eps-3	
-            #sol_copy = @views sol[:,t:t+t_eps+1]
-            #Lu_copy = @views Lu[:,t:t+t_eps+1]
-            Lu_copy = Lu[x0-x_eps:x0+x_eps,t:t+t_eps] #Lu[x0-x_eps:x0+x_eps,t:t+t_eps]
-            u = sol[x0,t]
-            integrated = (1/eps*dt*dx*sum(Lu_copy))^2
-            push!(data,(u,integrated))
-            end
-    end
-    return data
-end
-=#
- # module Volatility
